66c03189171eb4fe67facb5945516092ac835e5a
Ravina Dhamani <47536219+ravinadhamani@users.noreply.github.com>
2022-05-02 20:45:45 +0530

    Update Footer.js

--- files/Footer.js
+++ files/Footer.js
@@ -3,14 +3,14 @@ function Footer(){
     return(
 <footer>
 
-<div class="edyoda-container">
+<div className="edyoda-container">
     <h3>edyoda</h3>
   <p>about us</p>
   <p>contact us</p>
   <p>terms of use</p>
   <p>privacy policy</p>
 </div>
-<div class="resorce-contanier">
+<div className="resorce-contanier">
     <h3>resorces</h3>
   <p>learning path</p>
   <p>courses</p>
@@ -19,10 +19,10 @@ function Footer(){
   <p>edyoda stories</p>
   <p>university</p>
 </div>
-<div class="forEnterprices-container">
+<div className="forEnterprices-container">
     <h3>for enterprises</h3>
     <p>train your employees</p>
-     <div class="quiq-link">
+     <div className="quiq-link">
          <h3>quick links</h3>
     <p>learn and earn</p>
     <p>became a learning eligibler</p>
@@ -31,15 +31,15 @@ function Footer(){
       <p>certification</p>
 </div>
 </div>
-<div class="connect-container">
+<div className="connect-container">
     <h3>connect</h3>
     <p>2nd Floor #188, Survey No. - 123/1,
 Incubes Building Next to McDonalds,
 ITPL Main Rd, Brookefield,
 Bengaluru, Karnataka-560037,
 India</p>
-<p class="email"> <i class="fas fa-envelope"></i> hello@edyoda.com</p>
-<p class="social-media"> <i class="fas fa-map-marker-alt"></i> <i class="fab fa-twitter"></i> <i class="fab fa-facebook-f"></i> <i class="fab fa-linkedin-in"></i> </p>
+<p className="email"> <i class="fas fa-envelope"></i> hello@edyoda.com</p>
+<p className="social-media"> <i class="fas fa-map-marker-alt"></i> <i class="fab fa-twitter"></i> <i class="fab fa-facebook-f"></i> <i class="fab fa-linkedin-in"></i> </p>
 <small>© 2021
 zekeLabs Technologies Private Limited</small>
 </div>
